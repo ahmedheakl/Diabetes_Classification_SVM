7b575149495c79c6872cfe7a3748b87f602550b3
unknown <ahmedheakl2020@gmail.com>
2021-09-05 04:49:25 +0200

    built flask web server

--- predict.py
+++ predict.py
@@ -9,9 +9,10 @@ and returns whether if he has diabetes
 
 def get_patient_status(model, X, scaler_path='scaler.sav'):
     scaler = pickle.load(open(scaler_path, 'rb'))
-    X = scaler.transform(X)
-    X = np.array(X)
+
     # Reshaping the list to a 2-d array
-    X = X.reshape(1, -1)
+    X = np.array(X).reshape(1, -1)
+    X = scaler.transform(X)
+
     result = model.predict(X)[0]
-    return 'Diabetic' if result == 1 else 'Non-Diabetic'
+    return ('Srry, you have Diabetes', 1) if result == 1 else ("Congrats, you don't have diabetes!", 0)
